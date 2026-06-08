f773c84404e9c5d2e1db3d6048022ce790ba2913
Dhanusree-max <dhanusriabburi53@gmail.com>
2026-06-08 09:28:25 +0530

    first commit

---
+++ src/LinkedList/DLL.java
@@ -0,0 +1,221 @@
+package LinkedList;
+
+public class DLL {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    // Add at first
+    void addFirst(int value) {
+        Node node = new Node(value);
+
+        node.next = head;
+        node.prev = null;
+
+        if (head != null) {
+            head.prev = node;
+        }
+
+        head = node;
+
+        if (tail == null) {
+            tail = head;
+        }
+
+        size++;
+    }
+
+    // Add at last
+    void addLast(int value) {
+
+        if (tail == null) {
+            addFirst(value);
+            return;
+        }
+
+        Node node = new Node(value);
+
+        tail.next = node;
+        node.prev = tail;
+        tail = node;
+
+        size++;
+    }
+
+    // Insert at specific index
+    void insert(int index, int value) {
+
+        if (index == 0) {
+            addFirst(value);
+            return;
+        }
+
+        if (index == size) {
+            addLast(value);
+            return;
+        }
+
+        Node temp = get(index);
+
+        Node node = new Node(value);
+
+        node.next = temp;
+        node.prev = temp.prev;
+
+        temp.prev.next = node;
+        temp.prev = node;
+
+        size++;
+    }
+
+    // Delete first node
+    int deleteFirst() {
+
+        if (size == 0) {
+            return -1;
+        }
+
+        int val = head.value;
+
+        head = head.next;
+
+        if (head != null) {
+            head.prev = null;
+        } else {
+            tail = null;
+        }
+
+        size--;
+
+        return val;
+    }
+
+    // Delete last node
+    int deleteLast() {
+
+        if (size <= 1) {
+            return deleteFirst();
+        }
+
+        int val = tail.value;
+
+        tail = tail.prev;
+        tail.next = null;
+
+        size--;
+
+        return val;
+    }
+
+    // Delete at specific index
+    int delete(int index) {
+
+        if (index == 0) {
+            return deleteFirst();
+        }
+
+        if (index == size - 1) {
+            return deleteLast();
+        }
+
+        Node temp = get(index);
+
+        int val = temp.value;
+
+        temp.prev.next = temp.next;
+        temp.next.prev = temp.prev;
+
+        size--;
+
+        return val;
+    }
+
+    // Get node at index
+    Node get(int index) {
+
+        Node temp = head;
+
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+
+        return temp;
+    }
+
+    // Display forward
+    void display() {
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("End");
+    }
+
+    // Display reverse
+    void displayReverse() {
+
+        Node temp = tail;
+
+        while (temp != null) {
+            System.out.print(temp.value + " -> ");
+            temp = temp.prev;
+        }
+
+        System.out.println("Start");
+    }
+
+    // Node class
+    private class Node {
+
+        private int value;
+        private Node next;
+        private Node prev;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+
+        DLL dll = new DLL();
+
+        dll.addFirst(1);
+        dll.addFirst(2);
+        dll.addFirst(3);
+        dll.addFirst(4);
+        dll.addFirst(5);
+
+        dll.display();
+
+        dll.addLast(6);
+        dll.addLast(7);
+
+        dll.display();
+
+        dll.insert(2, 25);
+
+        dll.display();
+
+        dll.deleteFirst();
+
+        dll.display();
+
+        dll.deleteLast();
+
+        dll.display();
+
+        dll.delete(2);
+
+        dll.display();
+
+        System.out.println("Reverse Display:");
+        dll.displayReverse();
+    }
+}
