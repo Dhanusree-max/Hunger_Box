ef42a9478583a9f20a85e3a8f10c8012992a90f3
Dhanusree-max <dhanusriabburi53@gmail.com>
2026-06-15 16:47:42 +0530

    first commit

--- src/FileHandling/FH.java
+++ src/FileHandling/FH.java
@@ -1,8 +1,6 @@
 package FileHandling;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.util.Scanner;
 
 public class FH {
     public static void main(String[] args)throws IOException {
@@ -17,15 +15,32 @@ public class FH {
         bw.close();
         fw.close();*/
 
-        System.out.println("Starting my file");
+        /*System.out.println("Starting my file");
         String path="C:\\Users\\dhanu\\IdeaProjects\\Profit\\src\\FileHandling\\test.txt";
         File f=new File(path);
         FileWriter fw=new FileWriter(f,true);
         BufferedWriter bw=new BufferedWriter(fw);
         //fw.write("Hello");
-        bw.write(" HELLO AM ARUN");
+        Scanner sc=new Scanner(System.in);
+        String i=sc.nextLine();
+        bw.newLine();
+        bw.write(i);
         bw.close();
         fw.close();
-
+        */
+        System.out.println("Starting my file");
+        String path="C:\\Users\\dhanu\\IdeaProjects\\Profit\\src\\FileHandling\\test.txt";
+        File f2=new File("C:\\Users\\dhanu\\IdeaProjects\\Profit\\src\\FileHandling\\test2.txt");
+        File f1=new File(path);
+        FileInputStream fis1=new FileInputStream(f1);
+        FileOutputStream fis2=new FileOutputStream(f2);
+        int ch;
+        while((ch=fis1.read())!=-1){
+            System.out.print((char)ch);
+            fis2.write(ch);
+        }
+        fis1.close();
+        fis2.close();
+        //System.out.print(c);
     }
 }
